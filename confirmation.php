ccd24dc8fb0973d3a38af001a2fb5f12aecc7c27
MrPropreuuh <vincent.fougere@epsi.fr>
2023-05-07 20:26:02 +0200

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -1,10 +1,43 @@
+<?php
+if (isset($_GET['message'])) {
+    $message = $_GET['message'];
+
+    if ($message == 'success') {
+        $messageText = "Votre message a été envoyé avec succès !";
+    } elseif ($message == 'failed') {
+        $messageText = "Échec de l'envoi du message. Veuillez réessayer.";
+    } elseif ($message == 'wait') {
+        if (isset($_GET['time_to_wait'])) {
+            $timeToWait = $_GET['time_to_wait'];
+            $messageText = "Veuillez attendre $timeToWait secondes avant d'envoyer un nouveau message.";
+        } else {
+            $messageText = "Veuillez attendre un moment avant d'envoyer un nouveau message.";
+        }
+    } else {
+        $messageText = "Une erreur s'est produite lors de l'envoi du message.";
+    }
+} else {
+    $messageText = "Une erreur s'est produite lors de l'envoi du message.";
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Confirmation Page</title>
+    <title>Confirmation de l'envoi du message</title>
+    <style>
+        /* Ajoutez votre CSS personnalisé ici */
+        /* Par exemple, pour styliser le message, vous pouvez utiliser la classe "message" */
+        .message {
+            color: #333;
+            font-size: 18px;
+            margin-bottom: 20px;
+        }
+    </style>
 </head>
 <body>
-    <h2>Thank you for your message!</h2>
-    <p>We have received your message and will get back to you soon.</p>
+    <h2>Confirmation de l'envoi du message</h2>
+    <div class="message"><?php echo $messageText; ?></div>
+    <!-- Ajoutez d'autres éléments HTML si nécessaire -->
 </body>
 </html>
